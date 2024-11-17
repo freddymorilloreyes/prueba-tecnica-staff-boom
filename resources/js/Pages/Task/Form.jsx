187ece4f1dda28f32cb35c84982c313e8db791a3
Freddy Morillo <freddymr1616@gmail.com>
2024-11-17 15:36:47 -0500

    Ajustando el formulario

--- resources/js/Pages/Task/Form.jsx
+++ resources/js/Pages/Task/Form.jsx
@@ -4,6 +4,7 @@ import TextInput from "@/Components/TextInput.jsx"
 import InputError from "@/Components/InputError.jsx"
 import Checkbox from "@/Components/Checkbox.jsx"
 import PrimaryButton from "@/Components/PrimaryButton.jsx"
+import TextareaInput from "@/Components/TextareaInput.jsx";
 
 const Form = ({task = null, submitButton = 'Create'}) => {
 
@@ -16,7 +17,6 @@ const Form = ({task = null, submitButton = 'Create'}) => {
 
     const submit = (e) => {
         e.preventDefault()
-        // console.log(data)
         if (task) {
             patch(route('task.update', task))
         } else {
@@ -25,25 +25,38 @@ const Form = ({task = null, submitButton = 'Create'}) => {
     }
 
     return (
-        <form onSubmit={submit}>
-            <div>
-                <InputLabel htmlFor="title" value="Title"/>
-
-                <TextInput
-                    id="title"
-                    name="title"
-                    value={data.title}
-                    className="mt-1 block w-full"
-                    isFocused={true}
-                    onChange={(e) => setData('title', e.target.value)}
-                />
-
-                <InputError message={errors.title} className="mt-2"/>
+        <form onSubmit={submit} className="space-y-4">
+            {/* Fila para Title y Expiration Date */}
+            <div className="flex flex-wrap gap-4 md:flex-nowrap">
+                <div className="flex-1">
+                    <InputLabel htmlFor="title" value="Title"/>
+                    <TextInput
+                        id="title"
+                        name="title"
+                        value={data.title}
+                        className="mt-1 block w-full"
+                        isFocused={true}
+                        onChange={(e) => setData('title', e.target.value)}
+                    />
+                    <InputError message={errors.title} className="mt-2"/>
+                </div>
+                <div className="flex-1">
+                    <InputLabel htmlFor="expiration_date" value="Expiration Date"/>
+                    <TextInput
+                        id="expiration_date"
+                        type="date"
+                        name="expiration_date"
+                        value={data.expiration_date}
+                        className="mt-1 block w-full"
+                        isFocused={true}
+                        onChange={(e) => setData('expiration_date', e.target.value)}
+                    />
+                    <InputError message={errors.expiration_date} className="mt-2"/>
+                </div>
             </div>
             <div>
                 <InputLabel htmlFor="description" value="Description"/>
-
-                <TextInput
+                <TextareaInput
                     id="description"
                     name="description"
                     value={data.description}
@@ -51,23 +64,7 @@ const Form = ({task = null, submitButton = 'Create'}) => {
                     isFocused={true}
                     onChange={(e) => setData('description', e.target.value)}
                 />
-
-                <InputError message={errors.title} className="mt-2"/>
-            </div>
-            <div>
-                <InputLabel htmlFor="expiration_date" value="Expiration date"/>
-
-                <TextInput
-                    id="expiration_date"
-                    type="date"
-                    name="expiration_date"
-                    value={data.expiration_date}
-                    className="mt-1 block w-full"
-                    isFocused={true}
-                    onChange={(e) => setData('expiration_date', e.target.value)}
-                />
-
-                <InputError message={errors.title} className="mt-2"/>
+                <InputError message={errors.description} className="mt-2"/>
             </div>
             {task ? null : (
                 <div className="mt-4 block">
@@ -75,24 +72,19 @@ const Form = ({task = null, submitButton = 'Create'}) => {
                         <Checkbox
                             name="complete"
                             checked={data.complete}
-                            onChange={(e) =>
-                                setData('complete', e.target.checked)
-                            }
+                            onChange={(e) => setData('complete', e.target.checked)}
                         />
-                        <span className="ms-2 text-sm text-gray-600">
-                                                Complete
-                                            </span>
+                        <span className="ms-2 text-sm text-gray-600">Complete</span>
                     </label>
                 </div>
             )}
-
             <div className="mt-4 flex items-center justify-end">
-
                 <PrimaryButton className="ms-4" disabled={processing}>
                     {submitButton}
                 </PrimaryButton>
             </div>
         </form>
+
     )
 }
 
